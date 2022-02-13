8bed408e608ac57240d8615c2164b5f4685b8029
mukundkumarjha <mukundkumarjha@192.168.29.126>
2022-02-13 11:07:37 +0530

    Medplus-0000 @Mukund @Team added Product category

--- src/main/java/com/medSoft/repositories/ProductCategoryRepository.java
+++ src/main/java/com/medSoft/repositories/ProductCategoryRepository.java
@@ -1,9 +1,11 @@
 package com.medSoft.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.medSoft.models.ProductCategory;
 
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, UUID> {
 
 }
--- src/main/java/com/medSoft/services/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/medSoft/services/impl/ProductCategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.medSoft.services.impl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,8 +23,8 @@ public class ProductCategoryServiceImpl implements ProductCategoryService{
 	public String saveProductCategory(ProductCategory productCategory) {
 		
 		ProductCategory category = repo.save(productCategory);
-		log.info("Saved productCategory", category.getCategoryName());
-		return null != category && -1 != category.getId()?"Pass":"Fail";
+		log.info("Saved productCategory:{}", category);
+		return null != category ?"Pass":"Fail";
 	}
 
 	@Override
@@ -32,15 +33,15 @@ public class ProductCategoryServiceImpl implements ProductCategoryService{
 	}
 
 	@Override
-	public String modifyProductCategory(ProductCategory productCategory, long id) {
+	public String modifyProductCategory(ProductCategory productCategory, UUID id) {
 		productCategory.setId(id);
 		ProductCategory category = repo.save(productCategory);
 		log.info("Modified productCategory", category.getCategoryName());
-		return null != category && -1 != category.getId()?"Pass":"Fail";
+		return null != category ?"Pass":"Fail";
 	}
 
 	@Override
-	public ProductCategory findProductCategory(long id) {
+	public ProductCategory findProductCategory(UUID id) {
 		Optional<ProductCategory> category = repo.findById(id);
 		log.info("Fetched productCategory", category.isPresent()?category.get():null);
 		return category.isPresent()?category.get():null;
