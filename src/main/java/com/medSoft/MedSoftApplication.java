5b0257302916db280da52770de25c4de5d2cafa3
makkkkrand <makkkkrand@github.com>
2022-03-27 18:48:33 +0530

    MedSoft-0000 @Mukund @Team Removed web related unwnated content and added basic spring security

--- src/main/java/com/medSoft/MedSoftApplication.java
+++ src/main/java/com/medSoft/MedSoftApplication.java
@@ -4,15 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 //import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableSwagger2
 //@EnableAdminServer
+@OpenAPIDefinition(info = @Info(title = "MedSoft API", version = "1.0", description = "MedSoft Api Information"))
+@SecurityScheme(name = "medSoftApi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class MedSoftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedSoftApplication.class, args);
 	}
+
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
+//				.apis(RequestHandlerSelectors.basePackage("com.medSoft")).build().apiInfo(apiInfo());
+//	}
+//
+//	private ApiInfo apiInfo() {
+//		return new ApiInfo("MedSoft", "Api Documentation", "1.0", "urn:tos",
+//				new Contact("Mukundkumar Jha", "www.medsoftTechnologies.com", "contactus@medsoftTechnologies.com"),
+//				"Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+//	}
 
 }
--- src/main/java/com/medSoft/app/controller/ProductCategoryController.java
+++ src/main/java/com/medSoft/app/controller/ProductCategoryController.java
@@ -5,24 +5,28 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.medSoft.app.models.ProductCategory;
 import com.medSoft.app.services.ProductCategoryService;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
-@Controller
+
+@RestController
+@SecurityRequirement(name = "medSoftApi")
 public class ProductCategoryController {
 
 	@Autowired
 	ProductCategoryService productCategoryService;
 
-	@RequestMapping(value = "productCategory/save", method = RequestMethod.POST)
+	@PostMapping(value = "/productCategory/save")
 	public String storeProductCategory(@RequestBody ProductCategory productCategory,
 			@RequestHeader Map<String, String> header) {
 
@@ -30,18 +34,18 @@ public class ProductCategoryController {
 		return saveProductCategory;
 	}
 
-	@RequestMapping(value = "productCategory/fetchAll", method = RequestMethod.GET)
+	@GetMapping(value = "/productCategory/fetchAll")
 	public List<ProductCategory> fetchAllProductCategory() {
 		List<ProductCategory> findAllProductCategory = productCategoryService.findAllProductCategory();
 		return findAllProductCategory;
 	}
-	@RequestMapping(value = "productCategory/modify/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/productCategory/modify/{id}")
 	public String modifyProductCategory(@RequestBody ProductCategory productCategory, @PathVariable UUID id) {
 		String modifyProductCategory = productCategoryService.modifyProductCategory(productCategory, id);
 		return modifyProductCategory;
 	}
 	
-	@RequestMapping(value = "productCategory/fetch/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/productCategory/fetch/{id}")
 	public ProductCategory fetchProductCategory(@PathVariable UUID id) {
 		ProductCategory findProductCategory = productCategoryService.findProductCategory(id);
 		return findProductCategory;
